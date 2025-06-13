34fc2e3c567333adb92bbbacb3a51f171daefde7
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-06-13 14:46:01 +0200

    lade till fetch och random number och placerade fetch i useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 
 const Body = () => {
-const [meme, setMeme] = React.useState({
+const [meme, setMeme] = React.useState({ //for meme text
     topText: "",
     bottomText: "",
-    imgUrl: "https://html.com/wp-content/uploads/flamingo.webp"
     })
+const [selectedMeme, setselectedMeme] = React.useState([]) //for meme img
 
-    console.log("hi")
+const {topText, bottomText} = meme
 
-const {topText, bottomText, imgUrl} = meme
-
-const displayMeme = (event) => {
+const displayMeme = (event) => { //get the values the user is typing in input fields into state
   const {name, value} = event.currentTarget
   setMeme(prevMeme => ({
     ...prevMeme, [name]: value //name is used as a key in update object
   }));
 }
+
+const returnRandomNr = () => {
+    let random = Math.floor(Math.random() * 101)
+    return random
+}
+
+const getNewImage = () => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res=>res.json())
+    .then(data => {setselectedMeme(data.data.memes[returnRandomNr()])
+        console.log(data.data.memes)
+    })
+}
+
+React.useEffect(()=>{
+    getNewImage()
+},[])
+
+console.log(returnRandomNr())
+console.log("selectedMeme", selectedMeme)
+
+
 
   return (
         <main>
@@ -39,11 +59,11 @@ const displayMeme = (event) => {
                             onChange={displayMeme}
                         />
                     </label>
-                    <button>Get a new meme image 🖼</button>
+                    <button onClick={getNewImage}>Get a new meme image 🖼</button>
                 </div>
                 <div>
                     <div className="meme">
-                        <img src={imgUrl} />
+                        <img src={selectedMeme.url} alt={selectedMeme.name}/>
                         <p className="top">{topText}</p>
                         <p className="bottom">{bottomText}</p>
                     </div>
